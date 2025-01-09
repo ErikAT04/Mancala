0d35cfd303020da8d3bd74410e0dc6e1eab97140
Erik Amo <aterik.dev@gmail.com>
2025-01-09 10:49:19 +0100

    Ya funciona toda mi parte

--- app/src/main/java/androids/erikat/pruebamancala/OnePlayerGameView.kt
+++ app/src/main/java/androids/erikat/pruebamancala/OnePlayerGameView.kt
@@ -9,11 +9,47 @@ import androidx.appcompat.app.AppCompatActivity
 class OnePlayerGameView : AppCompatActivity() {
     lateinit var mibinding: ActivityOnePlayerGameBinding
     lateinit var listabotones: List<Button>
+
+    /**Función sobrescrita de creación de vista
+     *
+     * Inicia los componentes y los botones, además de iniciar el juego*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         iniciarComponentes()
+        iniciarBotones()
     }
 
+    /**Función de inicio de botones -> Les da a todos los botones una función
+     *
+     * Haciendo pruebas vi que se guarda en la memoria de cada botón la posición de la lista, es decir, si hago el listener en lista(0), lo que se referencie como indiceboton en este siempre va a tener valor 0
+     *
+     * Se hace de la siguiente manera:
+     *
+     *  - Se guarda el valor del botón y se pone 0 en su texto.
+     *  - Del siguiente botón a n puestos hacia la derecha siendo n el valor guardado, se suma 1 en cada puesto
+     * - Para controlar que no se sale de la lista, saco la posición como el resto de (indice + contador)/tamaño de lista, así las posiciones van de 0 al tamaño menos 1)
+     */
+    private fun iniciarBotones() {
+        for (indiceBoton in listabotones.indices) {
+            listabotones[indiceBoton].setOnClickListener {
+                try {
+                    var numSumas: Int = listabotones[indiceBoton].text.toString().toInt()
+                    listabotones[indiceBoton].text = "0"
+                    var numFinal = 0
+                    for (i in 1..numSumas) {
+                        numFinal = (indiceBoton + i) % listabotones.size
+                        var botonActual = listabotones[numFinal]
+                        botonActual.text = (botonActual.text.toString().toInt() + 1).toString()
+                    }
+                    cambiarEstadoBotones(numFinal)
+                } catch (e: Exception) {
+                    AlertDialog.Builder(this).setMessage(e.message).create().show()
+                }
+            }
+        }
+    }
+
+    /**Funcion de inicio de componentes*/
     private fun iniciarComponentes() {
         mibinding = ActivityOnePlayerGameBinding.inflate(layoutInflater)
 
@@ -26,50 +62,53 @@ class OnePlayerGameView : AppCompatActivity() {
             mibinding.semilla4,
             mibinding.rumaBt
         )
-
-        for (indiceBoton in listabotones.indices) {
-            listabotones[indiceBoton].setOnClickListener {
-                try {
-                    var numSumas: Int = listabotones[indiceBoton].text.toString().toInt()
-                    listabotones[indiceBoton].text = "0"
-                    var numFinal = 0
-                    for (i in 1..numSumas) {
-                        numFinal = (indiceBoton + i) % listabotones.size
-                        var botonActual = listabotones[numFinal]
-                        botonActual.text = (botonActual.text.toString().toInt() + 1).toString()
-                    }
-                    desactivarBotones(numFinal)
-                } catch (e: Exception) {
-                    AlertDialog.Builder(this).setMessage(e.message).create().show()
-                }
-            }
-            if (indiceBoton < 4) {
-                listabotones[indiceBoton].text = ((2..4).random().toString())
-            }
-        }
-        desactivarBotones(-1)
+        iniciarJuego()
     }
 
-    private fun desactivarBotones(num: Int) {
-        if (num in (0..3) && listabotones[num].text.toString().toInt() == 1) {
+    /**Función de inicio de juego
+     *
+     * Todos los botones salvo la ruma reciben un valor numérico entre 2 y 4*/
+    private fun iniciarJuego() {
+        mibinding.rumaBt.isEnabled = false
+        for (boton in listabotones.subList(0, 4)){
+            boton.text = ((3..5).random().toString())
+        }
+        cambiarEstadoBotones(4)
+    }
+
+    /**Función de desactivación de botones
+     * Hay distintas condiciones:
+     *   - Si el último botón modificado no es la ruma y el valor del botón es igual a 1 (estaba vacio), se termina el juego habiendo perdido
+     *   - Si el último botón modificado es es la ruma, se desbloquean todos los botones con valor (numero != 0)
+     *   - Si el numero es distinto a 4 y el boton de dicha posicion no es 1, se bloquean todos menos ese
+    */
+    private fun cambiarEstadoBotones(num: Int) {
+        if (num != 4 && listabotones[num].text.toString().toInt() == 1) { //Si el numero no es 4 y el boton de esa posicion es 1, se pierde
             for (boton in listabotones){
                 boton.isEnabled = false
             }
             AlertDialog.Builder(this).setMessage("¡Perdiste!").show()
-        } else {
-            for (boton in listabotones) {
-                boton.isEnabled = boton.text.toString().toInt() > 1 && boton != mibinding.rumaBt
-                if (num != -1 && boton.isEnabled && num != listabotones.indexOf(mibinding.rumaBt)) {
-                    boton.isEnabled = listabotones[num] == boton
-                }
+        } else if (num == 4) { //Si el numero es 4, se desbloquean todos los botones distintos a 0
+            for (boton in listabotones.subList(0, 4)){
+                boton.isEnabled = boton.text.toString().toInt() != 0
             }
-            var sumaBtts = 0
-            for (indice in listabotones.indices-1){
-                sumaBtts += listabotones[indice].text.toString().toInt()
+            comprobarBotones()
+        } else{ //Si el numero es distinto a 4 y el boton de dicha posicion no es 1, se bloquean todos menos ese
+            for (boton in listabotones.subList(0, 4)) {
+                boton.isEnabled = listabotones[num] == boton
             }
-            if(sumaBtts==0){
-                AlertDialog.Builder(this).setMessage("¡Ganaste!").show()
-            }
+            comprobarBotones()
         }
     }
+
+    /**Función de comprobación de botones: Mira si se ha terminado el juego (Las semillas no tienen valor)*/
+    private fun comprobarBotones() {
+        var sumaBtts = 0
+        for (indice in listabotones.indices-4){ //Se recorren todos los indices menos el numero 4 (la ruma)
+            sumaBtts += listabotones[indice].text.toString().toInt()
+        }
+        if(sumaBtts==0){
+            AlertDialog.Builder(this).setMessage("¡Ganaste!").show()
+        }
     }
+}
