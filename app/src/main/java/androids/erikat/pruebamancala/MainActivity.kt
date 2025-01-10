d2c9c1084ce07ad112f24005997cfc34bc8df445
Angel2doDAM <angel.manher@educa.jcyl.es>
2025-01-10 02:30:00 +0100

    Normas mal, dialogos bien

--- app/src/main/java/androids/erikat/pruebamancala/MainActivity.kt
+++ app/src/main/java/androids/erikat/pruebamancala/MainActivity.kt
@@ -1,19 +1,12 @@
 package androids.erikat.pruebamancala
 
-import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androids.erikat.pruebamancala.databinding.ActivityMainBinding
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var mibinding: ActivityMainBinding
-    lateinit var listabotones: List<Button>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         iniciarComponentes()
@@ -21,60 +14,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun iniciarComponentes() {
         mibinding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(mibinding.root)
+        val btnSolitario= mibinding.btnSolitario
+        val btnVariosJugadores= mibinding.btn2Jugadores
+        val btnNormas= mibinding.btnNormas
 
-        listabotones = listOf(
-            mibinding.semilla1,
-            mibinding.semilla2,
-            mibinding.semilla3,
-            mibinding.semilla4,
-            mibinding.rumaBt
-        )
-
-        for (indiceBoton in listabotones.indices) {
-            listabotones[indiceBoton].setOnClickListener {
-                try {
-                    var numSumas: Int = listabotones[indiceBoton].text.toString().toInt()
-                    listabotones[indiceBoton].text = "0"
-                    var numFinal = 0
-                    for (i in 1..numSumas) {
-                        numFinal = (indiceBoton + i) % listabotones.size
-                        var botonActual = listabotones[numFinal]
-                        botonActual.text = (botonActual.text.toString().toInt() + 1).toString()
-                    }
-                    desactivarBotones(numFinal)
-                } catch (e: Exception) {
-                    AlertDialog.Builder(this).setMessage(e.message).create().show()
-                }
-            }
-            if (indiceBoton < 4) {
-                listabotones[indiceBoton].text = ((2..4).random().toString())
-            }
+        // En función de que boton se pulse, nos enviará a la pantalla de juego en solitario
+        // o la pantalla de creación/selección de jugadores.
+        btnSolitario.setOnClickListener {
+            var intent= Intent(this, MancalaSolitarioActivity::class.java)
+            startActivity(intent)
         }
-        desactivarBotones(-1)
-    }
 
-    private fun desactivarBotones(num: Int) {
-        if (num in (0..3) && listabotones[num].text.toString().toInt() == 1) {
-            for (boton in listabotones){
-                boton.isEnabled = false
-            }
-            AlertDialog.Builder(this).setMessage("¡Perdiste!").show()
-        } else {
-            for (boton in listabotones) {
-                boton.isEnabled = boton.text.toString().toInt() > 1 && boton != mibinding.rumaBt
-                if (num != -1 && boton.isEnabled && num != listabotones.indexOf(mibinding.rumaBt)) {
-                    boton.isEnabled = listabotones[num] == boton
-                }
-            }
-            var sumaBtts = 0
-            for (indice in listabotones.indices-1){
-                sumaBtts += listabotones[indice].text.toString().toInt()
-            }
-            if(sumaBtts==0){
-                AlertDialog.Builder(this).setMessage("¡Ganaste!").show()
-            }
+        btnVariosJugadores.setOnClickListener {
+            var intent= Intent(this, CrearJugadoresActivity::class.java)
+            startActivity(intent)
         }
+
+        btnNormas.setOnClickListener {
+            var intent= Intent(this, NormasMancalaActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
-    }
+}
